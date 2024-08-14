8f926fc996f1d24ba78612822e2c56e7df4c0ac2
Raheel Ahmed <101556343+raheel-436@users.noreply.github.com>
2024-08-15 03:05:26 +0500

    'Js script Added'

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+const apiKEY = "5e55849049a352bcc03680430c989b2a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+
+let searchInput = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
+
+const defaultCity = "Berlin";
+
+// Fetch weather for default city on page load
+fetchWeather(defaultCity);
+
+async function fetchWeather(city) {
+  const respone = await fetch(apiUrl + city + `&appid=${apiKEY}`);
+  let data = await respone.json();
+  console.log(data);
+  displayWeather(data);
+}
+
+function displayWeather(data) {
+  let cityElement = document.querySelector(".city");
+  let tempElement = document.querySelector(".temp");
+  let humidityElement = document.querySelector(".humidity");
+  let windElement = document.querySelector(".wind");
+  let weatherIcon = document.querySelector(".weather-img");
+
+  cityElement.innerHTML = data.name;
+  tempElement.innerHTML = Math.round(data.main.temp) + "°c";
+  humidityElement.innerHTML = data.main.humidity + "%";
+  windElement.innerHTML = data.wind.speed + "km/h";
+
+  if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "./assets/clear.png";
+  } else if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "./assets/clouds.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "./assets/drizzle.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "./assets/rain.png";
+  } else if (data.weather[0].main == "Snow") {
+    weatherIcon.src = "./assets/snow.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "./assets/mist.png";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  let searchValue = searchInput.value;
+  fetchWeather(searchValue);
+  searchInput.value = "";
+});
